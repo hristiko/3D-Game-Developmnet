b8e634c76bea23f4a8db3e1fc23262f242cf074b
HK04 <hristiko@hotmail.com>
2026-04-08 00:19:24 +0200

    Small changes

--- Assets/Scripts/Level3WinChecker.cs
+++ Assets/Scripts/Level3WinChecker.cs
@@ -8,26 +8,15 @@ public class Level3WinChecker : MonoBehaviour
     public int requiredGoblinKills = 21;
     public string gameWonSceneName = "GameWon";
 
-    int startMaterials = 0;
     int killedGoblins = 0;
     bool isLoading = false;
-
-    void Start()
-    {
-        startMaterials = playerInventory.materialsAmount;
-    }
 
     void Update()
     {
         if (isLoading) return;
         if (playerInventory == null) return;
 
-        int collectedThisLevel = playerInventory.materialsAmount - startMaterials;
-
-        if (collectedThisLevel < 0)
-            collectedThisLevel = 0;
-
-        if (collectedThisLevel >= requiredMaterials && killedGoblins >= requiredGoblinKills)
+        if (playerInventory.materialsAmount >= requiredMaterials && killedGoblins >= requiredGoblinKills)
         {
             isLoading = true;
             SceneManager.LoadSceneAsync(gameWonSceneName);
